5f8725ab8e1c25cfa47724f30bfcdbe94b925908
VKI <you@example.com>
2012-10-27 12:43:25 +0300

    add start()

--- src/cwmp.erl
+++ src/cwmp.erl
@@ -1,131 +1,39 @@
-%%%-------------------------------------------------------------------
 %%% @author vlad <lib.aca55a@gmail.com>
 %%% @copyright (C) 2012, vlad
 %%% @doc
-%%%  Middle Manager for SOAP envelop processing.
-%%%  A standard XML-based syntax used here to encode remote procedure calls.
-%%%  Specifically SOAP 1.1, as specified in [9].
+%%%  Core functionality for ACS
 %%% @end
-%%% Created :  4 Aug 2012 by vlad <lib.aca55a@gmail.com>
-%%%-------------------------------------------------------------------
+%%% Created :  9 Oct 2012 by vlad <lib.aca55a@gmail.com>
+
 -module(cwmp).
 
--behaviour(gen_server).
+-author('author <author@example.com>').
+-export([start/0, start_link/0, stop/0]).
 
-%% API
--export([start_link/2]).
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-	 terminate/2, code_change/3]).
+%% @spec start_link() -> {ok,Pid::pid()}
+%% @doc Starts the app for inclusion in a supervisor tree
+start_link() ->
+    ensure_started(crypto),
+    cwmp_sup:start_link().
 
--define(SERVER, ?MODULE).
+%% @spec start() -> ok
+%% @doc Start the cwmp server.
+start() ->
+    ensure_started(crypto),
+    application:start(cwmp).
 
--record(state, {channel :: pid(),
-		process :: pid()}).
+%% @spec stop() -> ok
+%% @doc Stop the cwmp server.
+stop() ->
+    Res = application:stop(cwmp),
+    application:stop(crypto),
+    Res.
 
-%%%===================================================================
-%%% API
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link(Pid, Pid) -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
-start_link(Channel, Process) ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [Channel, Process], []).
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
-init([Channel, Process]) ->
-    {ok, #state{channel = Channel, process = Process}}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
